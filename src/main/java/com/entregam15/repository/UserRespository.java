4a214eedfd7614c84a6478300f0526ea17d1ed4e
Stefano <stefano.pola.fargas@gmail.com>
2021-01-18 19:36:47 +0100

    Creacio de UserRepository

---
+++ src/main/java/com/entregam15/repository/UserRespository.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.entregam15.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.entregam15.entity.User;
+
+/**
+ * @author stefano
+ *
+ */
+
+@Repository
+public interface UserRespository extends JpaRepository<User, Long>{
+
+	public Optional<User> findByUsername(String username);
+	
+}
