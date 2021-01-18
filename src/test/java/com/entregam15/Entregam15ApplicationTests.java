f5b842cebc2d20e44eb6d6f0a00d98120e79c80e
Stefano <stefano.pola.fargas@gmail.com>
2021-01-18 18:21:08 +0100

    Creacio del projecte

---
+++ src/test/java/com/entregam15/Entregam15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entregam15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Entregam15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
