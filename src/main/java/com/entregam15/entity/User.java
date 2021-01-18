839877d99aaba2c06aed148c38cbc47709d15636
Stefano <stefano.pola.fargas@gmail.com>
2021-01-18 18:51:00 +0100

    Creacio de l'entity User

---
+++ src/main/java/com/entregam15/entity/User.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.entregam15.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author stefano
+ *
+ */
+
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="USERS")
+public class User {
+
+	 @Id
+	 @Column (name ="ID")
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+	 private Long id;
+	 	 
+	 @Column(name="NAME", nullable = false)
+	 private String userName;
+	 
+	 @Column(name="PASSWORD", nullable = false)
+	 private String password;
+	 
+	 @Column(name="REGISTRATION DATE", nullable = false)
+	 private LocalDateTime registrationDate;
+	 
+	 @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	 private List<Game> listGames;
+}
