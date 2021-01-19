fe101fe761e8f143bbaafdfa88a108f8fe4986d2
Stefano <stefano.pola.fargas@gmail.com>
2021-01-19 14:58:26 +0100

    Creacio de la classe i metodes de GameMapper

---
+++ src/main/java/com/entregam15/mapper/GameMapper.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package com.entregam15.mapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.entregam15.dto.GameDTO;
+import com.entregam15.dto.GamesUserDTO;
+import com.entregam15.entity.Game;
+import com.entregam15.entity.User;
+
+
+/**
+ * @author stefano
+ *
+ */
+public class GameMapper extends AbstractMapper<Game, GameDTO> {
+   
+	@Override
+	public GameDTO fromEntity(Game entity) {
+		return GameDTO.builder()
+				.valueFirstDice(entity.getValueFirstDice())
+                .valueSecondDice(entity.getValueSecondDice())
+				.totalValue(entity.getTotalValue())
+				.build();
+	}
+	
+	
+
+	private List<GameDTO> fromAllEntities(List<Game> listGames) {
+		if (listGames == null)
+			return null;
+
+		List<GameDTO> listGamesDTO = listGames.stream().map(Game -> fromEntity(Game)).collect(Collectors.toList());
+
+		return listGamesDTO;
+	}
+	
+	
+	public GamesUserDTO fromAllEntitiesByUSer (User user, List<Game> listGames) {
+		
+		return GamesUserDTO.builder()
+				.userName(user.getUserName())
+                .listGamesByUser(fromAllEntities(listGames))				
+				.build();
+		
+	}
+	
+	
+	
+	
+}
