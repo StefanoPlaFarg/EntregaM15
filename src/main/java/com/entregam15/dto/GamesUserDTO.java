5bdc21c3d1472191f8e2f6a36f5d498175d3e818
Stefano <stefano.pola.fargas@gmail.com>
2021-01-19 13:22:30 +0100

    Creacio de la classe

---
+++ src/main/java/com/entregam15/dto/GamesUserDTO.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.entregam15.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GamesUserDTO {
+
+	private String userName;
+	private List<GameDTO> listGamesByUser;
+	
+}
