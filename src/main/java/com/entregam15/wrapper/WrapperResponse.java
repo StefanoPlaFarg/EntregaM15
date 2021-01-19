4306e52357f72c970de8123a7b6bec9cba4073e8
Stefano <stefano.pola.fargas@gmail.com>
2021-01-19 11:50:55 +0100

    Creacio de la classe WrapperResponse

---
+++ src/main/java/com/entregam15/wrapper/WrapperResponse.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.entregam15.wrapper;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class WrapperResponse<T> {
+	private boolean requestHandled;
+	private String messageToReturn;
+	private T objectToReturn;
+	
+	public ResponseEntity<WrapperResponse<T>> createResponse(){
+		return new ResponseEntity<>(this, HttpStatus.OK);
+	}
+	
+	public ResponseEntity<WrapperResponse<T>> createResponse(HttpStatus status){
+		return new ResponseEntity<>(this,status);
+	}
+}
