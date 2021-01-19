59017504ea9558f1e3955cc8a73ce873e2f572ee
Stefano <stefano.pola.fargas@gmail.com>
2021-01-19 15:21:10 +0100

    Eliminacio de metodes

--- src/main/java/com/entregam15/mapper/AbstractMapper.java
+++ src/main/java/com/entregam15/mapper/AbstractMapper.java
@@ -9,18 +9,5 @@ import java.util.stream.Collectors;
 public abstract class AbstractMapper<E, D> {
 
 	public abstract D fromEntity(E entity);
-	//public abstract E fromDTO(D dto);
-	
-	public List<D> fromEntity(List<E> entityList){
-		return entityList.stream()
-			.map(e -> fromEntity(e))
-			.collect(Collectors.toList());
-	}
-	
-	/*public List<E> fromDTO(List<D> dtos){
-		return dtos.stream()
-			.map(e -> fromDTO(e))
-			.collect(Collectors.toList());
-	}
-	*/
+
 }
