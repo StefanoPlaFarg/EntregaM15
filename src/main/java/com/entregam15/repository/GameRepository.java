3345060b47265079349e3ea2204ef32d76027b33
Stefano <stefano.pola.fargas@gmail.com>
2021-01-19 16:57:37 +0100

    Addicio del metode findByUser

--- src/main/java/com/entregam15/repository/GameRepository.java
+++ src/main/java/com/entregam15/repository/GameRepository.java
@@ -4,9 +4,12 @@
 package com.entregam15.repository;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.entregam15.entity.Game;
+import com.entregam15.entity.User;
 
 /**
  * @author stefano
@@ -16,6 +19,6 @@ import com.entregam15.entity.Game;
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long>{
 
-
+	public Optional<User> findByUser(User user);
 	
 }
