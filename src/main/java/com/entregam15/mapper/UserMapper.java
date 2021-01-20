ffdf79b33c9f202ab0a811e8f62455200c91bb40
Stefano <stefano.pola.fargas@gmail.com>
2021-01-20 11:46:50 +0100

    Modificacio del metode signUp

--- src/main/java/com/entregam15/mapper/UserMapper.java
+++ src/main/java/com/entregam15/mapper/UserMapper.java
@@ -3,6 +3,9 @@
  */
 package com.entregam15.mapper;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import com.entregam15.dto.SignupRequestDTO;
 import com.entregam15.dto.UserDTO;
 import com.entregam15.entity.User;
@@ -12,7 +15,10 @@ import com.entregam15.entity.User;
  *
  */
 public class UserMapper extends AbstractMapper<User, UserDTO>{
-
+   
+	private LocalDateTime regDate;
+	
+	
 	@Override
 	public UserDTO fromEntity(User entity) {
 		return UserDTO.builder()
@@ -30,9 +36,24 @@ public class UserMapper extends AbstractMapper<User, UserDTO>{
 	}
 
 	public User signup(SignupRequestDTO dto) {
+		
+		String userName;
+		
+		if (dto.getUsername().isEmpty()) {
+			
+			userName = "ANONIM";
+			
+		}else {
+			
+			userName = dto.getUsername();
+			
+		}
+		
+		
 		return User.builder()
-				.userName(dto.getUsername())
+				.userName(userName )
 				.password(dto.getPassword())
+				.registrationDate(regDate)
 				.build();
 	}
 	
