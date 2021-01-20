49b235d615e8e521c8c99b40c364748f8e5f8f9a
Stefano <stefano.pola.fargas@gmail.com>
2021-01-20 11:07:10 +0100

    Creacio dels metodes GameController referents a Game

--- src/main/java/com/entregam15/controller/GameController.java
+++ src/main/java/com/entregam15/controller/GameController.java
@@ -24,7 +24,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.entregam15.service.GameService;
+import com.entregam15.dto.*;
+import com.entregam15.entity.*;
+import com.entregam15.mapper.*;
+import com.entregam15.service.*;
+import com.entregam15.wrapper.*;
 
 
 
@@ -35,9 +39,98 @@ public class GameController {
 	@Autowired
 	private GameService gameService;
 	
-	private UserService users
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private GameMapper gameMapper;
+	
+	@Autowired
+	private TotalGamesMapper totalGamesMapper;
+	
+	@Autowired
+	private UserMapper userMapper;
 	
 	
+	//Users
+	@PostMapping(value = "POST/players/")
+	public void createUser() {
+		
+	}
+	
+	@PutMapping(value = "PUT/players/")
+    public void updateUser() {
+		
+	}
+	
+	//Game
+	
+	
+	@PostMapping(value = "POST/players/{id}/games")
+	public ResponseEntity<WrapperResponse<GameDTO>> play(@PathVariable(name="id") Long id){
+		
+		User user = userService.findById (id);		
+		Game game = gameService.playAndSaveGame(user);		
+		return new WrapperResponse<>(true, "success", gameMapper.fromEntity(game)).createResponse();
+	}
+	
+	@DeleteMapping(value = "DELETE/players/{id}/games")
+	public ResponseEntity<?> deleteGames(@PathVariable(name = "id") Long id) {
+		
+		User user = userService.findById(id);
+		gameService.deleteAllGamesAndRankingByUser(user);
+		return new WrapperResponse<>(true, "success", null).createResponse();
+	}
+	
+	@GetMapping(value = "GET/players")
+	public ResponseEntity<WrapperResponse<TotalAverageGamesDTO>> findAllRankings(
+			@RequestParam(name = "pageNumber", required = false, defaultValue = "0") int pageNumber,
+			@RequestParam(name = "pageSize", required = false, defaultValue = "5") int pageSize
+			){
+		
+		Pageable page = PageRequest.of(pageNumber, pageSize);
+		List<TotalGames> listTotalGames =gameService.findGroupRankings(page);
+		return new WrapperResponse<>(true, "success", totalGamesMapper.fromAllTotalGamesEntities(listTotalGames))
+				.createResponse();
+	}
+	
+	
+	@GetMapping(value = "GET/players/{id}/games")
+	public ResponseEntity<WrapperResponse<GamesUserDTO>> findAllGamesByUser(
+			@RequestParam(name = "pageNumber", required = false, defaultValue = "0") int pageNumber,
+			@RequestParam(name = "pageSize", required = false, defaultValue = "5") int pageSize
+			){
+		
+		Pageable page = PageRequest.of(pageNumber, pageSize);
+		User user = userService.findById(id);
+		
+		List<Game> listGamesByUser =gameService.findGamesByUser(user, page);
+		return new WrapperResponse<>(true, "success", gameMapper.fromAllEntitiesByUser(user, listGamesByUser))
+				.createResponse();
+	}
+	
+	@GetMapping(value = "GET/players/ranking")
+      public ResponseEntity<WrapperResponse<AverageRankingDTO>> findAverageRanking() {
+		
+		double averageRanking = gameService.findAverageRanking();
+		return new WrapperResponse<>(true, "success", totalGamesMapper.fromAverageRanking(averageRanking)).createResponse();
+	}
+
+	
+	@GetMapping(value = "GET/players/ranking/loser")
+     public ResponseEntity<WrapperResponse<UserDTO>> findLoser() {
+		
+		User loser = gameService.findLoser();
+		return new WrapperResponse<>(true, "success", userMapper.fromEntity(loser)).createResponse();
+	}
+	
+	
+	@GetMapping(value = "GET/players/ranking/winner")
+       public ResponseEntity<WrapperResponse<UserDTO>> findWinner() {
+		
+		User winner = gameService.findWinner();
+		return new WrapperResponse<>(true, "success", userMapper.fromEntity(winner)).createResponse();
+	}
 	
 
 }
