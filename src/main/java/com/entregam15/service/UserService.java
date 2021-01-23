8add7a76b4581ea9e2760f7bb0721af82c97465e
Stefano <stefano.pola.fargas@gmail.com>
2021-01-23 09:55:24 +0100

    #CREATE: Creacio del metode login

--- src/main/java/com/entregam15/service/UserService.java
+++ src/main/java/com/entregam15/service/UserService.java
@@ -13,9 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.entregam15.dto.LoginRequestDTO;
+import com.entregam15.dto.LoginResponseDTO;
 import com.entregam15.dto.UserDTO;
 import com.entregam15.entity.*;
 import com.entregam15.exception.*;
+import com.entregam15.mapper.UserMapper;
 import com.entregam15.repository.*;
 import com.entregam15.validator.UserValidator;
 
@@ -31,6 +34,9 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class UserService {
 
+	@Autowired
+	UserMapper userMapper;
+	
 	@Autowired
 	private UserRespository userRepository;
 	
@@ -99,6 +105,29 @@ public class UserService {
 			log.error(e.getMessage(), e);
 			throw new GeneralServiceException(e.getMessage(), e);
 		}
+	}
+	
+	
+	public LoginResponseDTO login (LoginRequestDTO loginRequestDTO ) {
+	try {
+           
+		User user= userRepository.findByUserName(loginRequestDTO.getUserName())
+				.orElseThrow(() -> new ValidateServiceException ("The user or the password are incorrect") );
+			
+		if (!user.getPassword().equals(loginRequestDTO.getPassword())) throw new ValidateServiceException ("The user or the password are incorrect") ;
+			
+			
+		return new LoginResponseDTO(userMapper.fromEntity(user),"TOKEN");
+			
+		} catch (ValidateServiceException | NoDataFoundException e) {
+			log.info(e.getMessage(), e);
+			throw e;
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+			throw new GeneralServiceException(e.getMessage(), e);
+		}
+		
+		
 	}
 	
 }
