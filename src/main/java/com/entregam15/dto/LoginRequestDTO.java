755356696c317c357e64d30c8dadc04bafadf191
Stefano <stefano.pola.fargas@gmail.com>
2021-01-23 09:54:28 +0100

    #CREATE: Creacio de la classe LoginRequestDTO

--- src/main/java/com/entregam15/dto/LoginRequestDTO.java
+++ src/main/java/com/entregam15/dto/LoginRequestDTO.java
@@ -7,6 +7,21 @@ package com.entregam15.dto;
  * @author stefano
  *
  */
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequestDTO {
 
+	private String userName;
+	private String password;
+	
 }
