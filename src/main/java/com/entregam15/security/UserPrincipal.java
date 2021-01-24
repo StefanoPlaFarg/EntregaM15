020dd19119f5d9fff89fd9a0b639d840c6f93a62
Stefano <stefano.pola.fargas@gmail.com>
2021-01-24 11:13:04 +0100

    #CREATE: Creacio de UserPrincipal

---
+++ src/main/java/com/entregam15/security/UserPrincipal.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package com.entregam15.security;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.entregam15.entity.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserPrincipal implements UserDetails {
+	
+	private User user;
+	private Collection<? extends GrantedAuthority> authorities;
+	
+	public static UserPrincipal create(User user) {
+		List<GrantedAuthority> authorities = Collections
+				.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+		return new UserPrincipal(user, authorities);
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getUserName();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+	
+	//Per aconseguir un usuari de SecurityContextHolder (L'usuari que ha s'ha autentificat i se l'ha autoritzat a utilitzar l'API)
+	public static User getCurrentUser() {
+		UserPrincipal principal = (UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		return principal.getUser();
+	}
+}
