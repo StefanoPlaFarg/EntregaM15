37014a1ebcb775bbb1067ce054cbbda7f55f5e52
Stefano <stefano.pola.fargas@gmail.com>
2021-01-24 11:18:52 +0100

    #CREATE: createPasswordEncoder

--- src/main/java/com/entregam15/configuration/SecurityConfig.java
+++ src/main/java/com/entregam15/configuration/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.entregam15.security.RestAuthenticationEntryPoint;
@@ -23,6 +25,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	public TokenAuthentificationFilter createTokenAuthenticationFilter() {
 		return new TokenAuthentificationFilter(); 
 	}
+	
+	@Bean
+	public PasswordEncoder createPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
